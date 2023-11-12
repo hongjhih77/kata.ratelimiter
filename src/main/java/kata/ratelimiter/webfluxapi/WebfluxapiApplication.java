d0d2d5edf67ddc204784d19cf63094c0b2c87d31
HongJhihLin <>
2023-11-12 13:40:15 +0800

    spring webflux initialization

---
+++ src/main/java/kata/ratelimiter/webfluxapi/WebfluxapiApplication.java
@@ -0,0 +1,13 @@
+package kata.ratelimiter.webfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxapiApplication.class, args);
+	}
+
+}
