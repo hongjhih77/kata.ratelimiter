2a0ab849fae7c869b5d23849b7d6f5a7aec91078
HongJhihLin <>
2023-11-14 14:59:06 +0800

    InMemoryRequestLimiterTest : multi-thread test

--- src/test/java/kata/ratelimiter/webfluxapi/ratelimiter/InMemoryRequestLimiterTest.java
+++ src/test/java/kata/ratelimiter/webfluxapi/ratelimiter/InMemoryRequestLimiterTest.java
@@ -1,7 +1,10 @@
 package kata.ratelimiter.webfluxapi.ratelimiter;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +22,43 @@ class InMemoryRequestLimiterTest {
         assertThat(requestLimiter.tryAcquire(key, timeFrameSeconds, permits)).isTrue();
         assertThat(requestLimiter.tryAcquire(key, timeFrameSeconds, permits)).isTrue();
 
+        assertThat(requestLimiter.tryAcquire(key, timeFrameSeconds, permits)).isFalse();
+    }
+
+    @RepeatedTest(3)
+    @DisplayName("Concurrency Test")
+    void tryAcquireConcurrency() throws InterruptedException {
+        InMemoryRequestLimiter requestLimiter = new InMemoryRequestLimiter();
+
+        var key = "key";
+        int timeFrameSeconds = 60;
+        int permits = 100;
+
+        AtomicBoolean t1Acquired = new AtomicBoolean(true);
+        var t1 = new Thread(() -> {
+            for (int i = 1; i <= 50; i++) {
+                if (!requestLimiter.tryAcquire(key, timeFrameSeconds, permits)) {
+                    t1Acquired.set(false);
+                }
+            }
+        });
+
+        AtomicBoolean t2Acquired = new AtomicBoolean(true);
+        var t2 = new Thread(() -> {
+            for (int i = 1; i <= 50; i++) {
+                if (!requestLimiter.tryAcquire(key, timeFrameSeconds, permits)) {
+                    t2Acquired.set(false);
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        //check the requests from [1..100] are all succeed.
+        assertThat(t1Acquired.get() && t2Acquired.get()).isTrue();
+        //The last request should be failed.
         assertThat(requestLimiter.tryAcquire(key, timeFrameSeconds, permits)).isFalse();
     }
 }
